3fbd32248cb512d2d8144140cd8527aa7f4e4372
alexsandercoelho <60738527+alexsandercoelho@users.noreply.github.com>
2020-07-27 02:29:38 -0300

    3 dia

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FiLogIn} from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
 import './styles.css'
 
@@ -17,10 +18,10 @@ const Home = () => {
           <h1>Seu marketplace de coleta de residuos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
         
-          <a href ="/cadastro"> 
+          <Link to ="/create-point"> 
             <span> <FiLogIn /> </span> 
             <strong>Cadastre um ponto de Coleta</strong>
-          </a>
+          </Link>
 
         </main>
 
