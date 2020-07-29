c72f7173f43c37be694fb3af517faa398c6f1936
alexsandercoelho <60738527+alexsandercoelho@users.noreply.github.com>
2020-07-28 21:59:36 -0300

    Api Cidade Estado

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,37 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker} from 'react-leaflet'; 
 import api from '../../services/api';
+import axios from 'axios';
+
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
-import { tileLayer } from 'leaflet';
 
-interface Item {
-  id: number;
-  title: string;
-  image_url: string;
-}
+  interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+  }
+
+  interface IBGEUFResponse {
+    sigla: string;
+  }
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
-  
+  const [ufs, setUfs] = useState<string[]>([]);
+
+  const [selectedUF, setselectedUF] = useState('0');
+
+
+
   useEffect(() => {
     api.get('items').then(response => {
       setItems(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then
+    (response => { 
+      const ufInitials = response.data.map(uf => uf.sigla);
+      setUfs(ufInitials);
+    });
+  }, []);
+  //Vincular uf a cidade,sempre que a UF mudar.
+  useEffect(() => {
+
+    }, []); 
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+      event.target.
+    
+    }
+
   return (
     <div id="page-create-point">
       <header>
-        <img src={logo} alt="Ecoleta" />
+      <img src={logo} alt="Ecoleta" />
 
-        <Link to="/">
-          <FiArrowLeft />
-        Voltar para home
-        </Link>
+      <Link to ="/">
+        <FiArrowLeft />
+      </Link>
       </header>
 
       <form>
@@ -89,8 +116,12 @@ const CreatePoint = () => {
           <div className="field group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
+                <select name="uf" id="uf" onChange={handleSelectUf}>
                 <option value="0">Selecione uma UF</option>
+                {ufs.map(uf => (
+                  <option key={uf} value={uf}>{uf}</option>
+                ))}
+             
               </select>
             </div>
             <div className="field">
@@ -107,17 +138,16 @@ const CreatePoint = () => {
             <h2>Ítens de Coleta</h2>
             <span>Selecione um ou mais ìtens abaixo</span>
           </legend>
-          
-          <ul className="items-grid">
+            <ul className="items-grid">
             {items.map(item => (
-            <li>
-              <img src={item.image_url} alt={item.title} />
-              <span>{item.title}</span>
+             <li key={item.id}>
+            <img src={item.title} alt={item.title} />
+            <span>{item.title}</span>
             </li>
           ))}
           </ul>
         </fieldset>
-      
+                     
         <button type="submit">
           Cadastrar ponto de Coleta
         </button>
