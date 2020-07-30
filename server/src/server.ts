3ad40c93105d635b851ee40963f75d4e9caa8692
alexsandercoelho <60738527+alexsandercoelho@users.noreply.github.com>
2020-07-29 23:25:07 -0300

    mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(cors({origin: true, credentials: true}));
+app.use(cors());
 
 app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')));
 
